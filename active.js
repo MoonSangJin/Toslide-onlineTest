aafd328524779816f049403918895bdf3626d6d4
MoonSangJin <1128msj@naver.com>
2021-02-09 14:28:17 +0900

    feat/perStudent Page

--- active.js
+++ active.js
@@ -1,5 +1,11 @@
 let dropdownImage = document.querySelectorAll('#dropdownImage');
-let dropdownMenu = document.querySelectorAll('.dropdownMenu');
+let dropdownTitle = document.querySelectorAll('.dropdownTitle');
+const perProgramTable = document.getElementById('perProgramTable');
+const perStudentTable = document.getElementById('perStudentTable');
+const perProgram = document.querySelector('.perProgram');
+const perStudent = document.querySelector('.perStudent');
+const sortClassPerProgram = document.querySelector('.sortClass1');
+const sortClassPerStudent = document.querySelector('.sortClass2');
 
 const openDropdownMenu = (e) => {
   if (e.target.id !== 'clicked') {
@@ -7,26 +13,43 @@ const openDropdownMenu = (e) => {
 
     const newActiveObject1 = document.createElement('tr');
     const newActiveObject2 = document.createElement('tr');
+    // const newActiveObject3 = document.createElement('tr');
+
     newActiveObject1.innerHTML = ` 
   <td align="center" rowspan="2">
     <img class="imgSize" src="./asset/img-2021/inner_arrow.png" />
   </td>
-  <td class="usageTime">사용시간</td>
-<td class="name">이름</td>
-<td class="programName">프로그램</td>
+  <td class="tableTitle">사용시간</td>
+<td class="tableTitle">이름</td>
+<td class="tableTitle">프로그램</td>
 `;
     newActiveObject2.innerHTML = `
   <td class="usageTime">00:00:00</td>
   <td class="name">B611061 문상진</td>
   <td class="programName">Kakaotalk.exe</td>`;
-    newActiveObject1.setAttribute('class', 'dropdownMenu');
+
+    //   newActiveObject3.innerHTML = `
+    // <td align="center">
+    //   <img class="imgSize" src="./asset/img-2021/inner_arrow.png" />
+    // </td>
+    // <td class="usageTime">00:00:00</td>
+    // <td class="name">B611061 문상진</td>
+    // <td class="programName">Kakaotalk.exe</td>`;
+
+    newActiveObject1.setAttribute('class', 'dropdownTitle');
     newActiveObject2.setAttribute('class', 'dropdownMenu');
+    // newActiveObject3.setAttribute('class', 'dropdownMenu');
 
     tr.after(newActiveObject1);
     newActiveObject1.after(newActiveObject2);
+    // newActiveObject2.after(newActiveObject3);
     e.target.setAttribute('src', './asset//img-2021/X_or_Close_Black.png');
     e.target.setAttribute('id', 'clicked');
   } else {
+    let dropdownTitle = document.querySelectorAll('.dropdownTitle');
+    dropdownTitle.forEach((i) => {
+      i.classList.add('hidden');
+    });
     let dropdownMenu = document.querySelectorAll('.dropdownMenu');
     dropdownMenu.forEach((i) => {
       i.classList.add('hidden');
@@ -45,5 +68,28 @@ const findAllDropdown = () => {
     dropdownImage[i].addEventListener('click', openDropdownMenu);
   }
 };
+
+const openPerStudentTable = () => {
+  perStudentTable.classList.remove('hidden');
+  perProgramTable.classList.add('hidden');
+
+  sortClassPerStudent.style.background = '#ffff';
+  perStudent.style.color = 'black';
+  sortClassPerProgram.style.background = '#f4f5f6';
+  perProgram.style.color = '#7c7d80';
+};
+
+const openPerProgramTable = () => {
+  perProgramTable.classList.remove('hidden');
+  perStudentTable.classList.add('hidden');
+
+  sortClassPerProgram.style.background = '#ffff';
+  perProgram.style.color = 'black';
+  sortClassPerStudent.style.background = '#f4f5f6';
+  perStudent.style.color = '#7c7d80';
+};
+
+perProgram.addEventListener('click', openPerProgramTable);
+perStudent.addEventListener('click', openPerStudentTable);
 
 findAllDropdown();
